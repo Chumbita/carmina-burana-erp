960885484e05536d2a03e8ab4cf6e6acaa6c1433
tomas14k <tomyamh123@gmail.com>
2026-01-12 16:45:39 -0300

    feat: base layout con shadcn sidebar, header y ui components

--- frontend/src/components/layout/AppLayout.jsx
+++ frontend/src/components/layout/AppLayout.jsx
@@ -1,22 +1,27 @@
 import { Outlet } from "react-router-dom";
 import { useState } from "react";
-import Sidebar from "./Sidebar";
+//import Sidebar from "./Sidebar";
 import Header from "./Header";
+import { AppSidebar } from "./app-sidebar"
+
+import {
+  SidebarInset,
+  SidebarProvider,
+} from "@/components/ui/sidebar"
+
 
 export default function AppLayout() {
   const [collapsed, setCollapsed] = useState(false);
 
   return (
-    <div className="flex bg-red-500 h-screen">
-      <Sidebar collapsed={collapsed} />
-
-      <div className="flex flex-col flex-1">
-        <Header onToggleSidebar={() => setCollapsed(!collapsed)} />
-
-        <main className="flex-1 p-4 overflow-auto">
-          <Outlet />
-        </main>
-      </div>
-    </div>
+        <SidebarProvider>
+            <AppSidebar />
+            <SidebarInset>
+            <Header/>
+                <main className="flex-1 p-4 overflow-auto">
+                  <Outlet />
+                </main>          
+            </SidebarInset>
+        </SidebarProvider>
   );
 }
--- frontend/src/components/layout/Header.jsx
+++ frontend/src/components/layout/Header.jsx
@@ -1,11 +1,39 @@
-
+import { Separator } from "@/components/ui/separator"
+import {
+  SidebarTrigger,
+} from "@/components/ui/sidebar"
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb"
 
 export default function Header({ onToggleSidebar }) {
   return (
-    <header className="h-14 border-b flex items-center px-4">
-      <button onClick={onToggleSidebar}>
-        ☰
-      </button>
-    </header>
+        <header className="flex h-14 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+                   <div className="flex items-center gap-2 px-4">
+                        <SidebarTrigger className="-ml-1" />
+                        <Separator
+                        orientation="vertical"
+                        className="mr-2 data-[orientation=vertical]:h-4"
+                        />
+                        <Breadcrumb>
+                        <BreadcrumbList>
+                            <BreadcrumbItem className="hidden md:block">
+                            <BreadcrumbLink href="#">
+                                Building Your Applicationasd
+                            </BreadcrumbLink>
+                            </BreadcrumbItem>
+                            <BreadcrumbSeparator className="hidden md:block" />
+                            <BreadcrumbItem>
+                            <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+                            </BreadcrumbItem>
+                        </BreadcrumbList>
+                        </Breadcrumb>
+                    </div>
+                </header>
   );
 }
