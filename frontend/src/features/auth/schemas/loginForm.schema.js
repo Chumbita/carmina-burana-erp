25cc5ff34100123eebac3372cbba6f4277edce4b
Chumbita <chumbitalexis@gmail.com>
2026-01-12 16:52:51 -0300

    Feature: UI/UX Login de usuario

---
+++ frontend/src/features/auth/schemas/loginForm.schema.js
@@ -0,0 +1,11 @@
+import { z } from "zod";
+
+export const LoginSchema = z.object({
+    username: z
+        .string()
+        .min(3, { message: "El nombre de usuario debe tener al menos 3 caracteres." })
+        .max(20, { message: "El nombre de usuario no puede superar los 20 caracteres." }),
+    password: z
+        .string()
+        .min(4, { message: "La contraseña debe tener al menos 6 caracteres." })
+})
