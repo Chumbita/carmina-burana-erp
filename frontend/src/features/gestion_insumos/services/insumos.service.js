d40d246cfb2996bb9e908336bcee2c0f8aad5151
tomas14k <tomyamh123@gmail.com>
2026-01-21 19:22:32 -0300

    feat: modelo de datos provisorio. Navegacion a detalle de insumo

--- frontend/src/features/gestion_insumos/services/insumos.service.js
+++ frontend/src/features/gestion_insumos/services/insumos.service.js
@@ -1,6 +1,6 @@
 let INSUMOS = [
 {
-      id: 1,
+      id: "malta",
       nombre: "Malta",
       categoria: "Perecedero",
       unidadMedida: "kg",
@@ -8,8 +8,8 @@ let INSUMOS = [
       stockTotal: 120
     },
     {
-      id: 2,
-      nombre: "Harina",
+      id: "lupulo",
+      nombre: "Lúpulo",
       categoria:"perecedero",
       unidadMedida: "kg",
       stockMinimo: 10,
@@ -33,5 +33,72 @@ export async function createInsumo(data) {
   INSUMOS = [...INSUMOS, nuevoInsumo]
 
   return Promise.resolve(nuevoInsumo)
+}
+
+
+//insumosDetail
+const INSUMOS_DETALLE = [
+  {
+    id: "malta",
+    nombre: "Malta",
+    categoria: "Perecedero",
+    unidadMedida: "kg",
+    stockTotal: 120,
+    stockMinimo: 30,
+    insumos: [
+      {
+        id: "malta-pilsen",
+        nombre: "Malta Pilsen",
+        marca: "Patagonia",
+        stockTotal: 80,
+        stockMinimo: 40,
+        lotes: [
+          {
+            id: "L001",
+            numeroLote: "MP-2024-089",
+            cantidadDisponible: 50,
+            fechaVencimiento: "2025-06-15",
+            estado: "disponible",
+          },
+        ],
+      },
+    ],
+  },
+  {
+        
+    id: "lupulo",
+    nombre: "Lúpulo",
+    categoria: "Perecedero",
+    unidadMedida: "kg",
+    stockTotal: 234,
+    stockMinimo: 30,
+    insumos: [
+            {
+        id: "lupulo-las-sierras",
+        nombre: "Lúpulo las sierras",
+        marca: "Aguila guerrera",
+        stockTotal: 180,
+        stockMinimo: 20,
+        lotes: [
+          {
+            id: "L001",
+            numeroLote: "MP-2024-089",
+            cantidadDisponible: 50,
+            fechaVencimiento: "2025-06-15",
+            estado: "disponible",
+          },
+        ],
+      },
+    ],
+  }
+];
+
+export function getInsumoById(id) {
+  return new Promise((resolve) => {
+    
+      const insumo = INSUMOS_DETALLE.find((i) => i.id === id);
+      resolve(insumo || null);
+   
+  });
 }
 
