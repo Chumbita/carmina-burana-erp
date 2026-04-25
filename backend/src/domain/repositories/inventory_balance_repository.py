25b90015d199a19280a33bad71c4f662e6ae9ee0
Chumbita <chumbitalexis@gmail.com>
2026-04-25 13:08:08 -0300

    feature: interfaz del balance de inventario
    Métodos definidos:
    - get_by_lot() -> para obtener el balance por lote.
    - save() -> para persistir el estado de la entidad. La lógica si la entidad debe registrarse como nueva o simplemente actualizar sus atributos se delegan al repositorio en la capa de infraestructura.

---
+++ backend/src/domain/repositories/inventory_balance_repository.py
@@ -0,0 +1,24 @@
+# ══════════════════════════════════════════════════════════════════════════════
+# INTERFAZ DEL BALANCE DE INVENTARIO
+# ══════════════════════════════════════════════════════════════════════════════
+
+from typing import Protocol, Optional
+
+from src.domain.entities.inventory_balance import InventoryBalance
+
+class IInventoryBalanceRepository(Protocol):
+
+    async def get_by_lot(self, item_id: int, lot_id: int) -> InventoryBalance | None:
+        """ 
+        Obtiene el balance por lote.
+        """
+        ...
+
+    
+    async def save(self, balance: InventoryBalance) -> None:
+        """
+        Persiste el estado actual de la entidad:
+        -> Se insertará un nuevo registro en caso de ser una entidad nueva.
+        -> Se actualizará su registro en caso de que la entidad ya exista.
+        """
+        ...
