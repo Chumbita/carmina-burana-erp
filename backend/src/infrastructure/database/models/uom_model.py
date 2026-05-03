3dec6cfd8dddb5eab0a535c6fac2e2864211f50a
Carlos Olivera <carlitosolivera1510@gmail.com>
2026-05-02 21:15:46 -0300

    Modificación del modelo de uom

--- backend/src/infrastructure/database/models/uom_model.py
+++ backend/src/infrastructure/database/models/uom_model.py
@@ -1,9 +1,13 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Boolean, Numeric
 from src.infrastructure.database.base import Base
 
 class UomModel(Base):
     __tablename__ = "uom"
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(50), nullable=False, unique=True)
-    abbreviation = Column(String(10), nullable=False, unique=True)
+    symbol = Column(String(10), nullable=False)
+    uom_type = Column(String(20), nullable=False)
+    factor_to_base = Column(Numeric(24, 10), nullable=True)
+    is_base = Column(Boolean, nullable=False, default=False)
+    
