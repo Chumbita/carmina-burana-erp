4f992031cf4d80826925a1c9a23d6a930140a6cb
Chumbita <chumbitalexis@gmail.com>
2026-05-07 10:35:17 -0300

    feat: definición del método principal para la actualización de los datos de un item + item especializado.

---
+++ backend/src/application/use_cases/item/update_item_use_case.py
@@ -0,0 +1,68 @@
+# ══════════════════════════════════════════════════════════════════════════════
+# CASO DE USO PARA LA ACTUALIZACIÓN DE UN ITEM Y/O ITEM ESPECIALIZADO
+# ══════════════════════════════════════════════════════════════════════════════
+
+from typing import Optional
+
+from src.application.interfaces.specialized_item_updater import SpecializedItemUpdater
+from src.application.dtos.items.item_commands_dtos import UpdateItemCommand
+from src.application.dtos.items.item_responses_dtos import ItemResponse
+from src.domain.repositories.item_repository import IItemRepostory
+from src.domain.exceptions.item_exceptions import ItemNotFoundException, ItemAlreadyDeletedException, SpecializedItemUpdateException
+
+class UpdateItemUseCase():
+    def __init__(
+        self,
+        item_repository: IItemRepostory,
+        specialized_updater: Optional[SpecializedItemUpdater] = None
+    ):
+        self._item_repository = item_repository
+        self._specialized_updater= specialized_updater
+    
+    async def execute(self, command: UpdateItemCommand):
+        """ 
+        FLUJO:
+        1. Verificar la existencia del item mediante ID.
+        2. Verificar que el item se encuentre activo. Items con status=INACTIVE o DELETED rechazan la operación.
+        3. Si hay campos bases, se aplican los cambios en la entidad. 
+        4. Persistir los cambios en la base de datos (sin realizar commit).
+        5. Si hay specialized_data con valores válidos y el puerto inyectado, se delega la actualización del registro especializado.
+        6. Retornar Item + Specialized Item.
+        """
+        
+        # Paso 1 y 2: verificar existencia y estado.
+        item = self._item_repository.get_by_id(command.item_id)
+        
+        if item is None:
+            raise ItemNotFoundException(command.item_id)
+        if item.is_deleted():
+            raise ItemAlreadyDeletedException(command.item_id)
+        
+        # Paso 3: actualizar atributos de la entidad.
+        if command.has_base_changes:
+            item.update(command)
+        
+        # Paso 4: persistir cambios de la entidad en la db.
+        updated_item = await self._item_repository.save(item)
+        
+        # Paso 5: Delegar la actualización del item especializado
+        if self._specialized_updater is not None and command.has_specialized_changes:
+            try:
+                await self._specialized_updater.update(
+                    item_id=command.item_id,
+                    specialized_data=command.specialized_data
+                )
+            except SpecializedItemUpdateException:
+                raise
+            
+            except Exception as exc:
+                raise SpecializedItemUpdateException(str(exc)) from exc
+ 
+        return ItemResponse.from_entity(updated_item)
+                
+                
+        
+            
+        
+           
+        
