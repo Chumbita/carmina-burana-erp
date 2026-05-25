3118d4ad44e68520200b8214043a0cf07646b930
Aldo <pachjsiciliano@gmail.com>
2026-05-25 20:13:25 -0300

    feat(supplyService): implementar acceso a GET y POST /supplies

---
+++ frontend/src/features/Inventario/gestion_insumos/services/supplyService.js
@@ -0,0 +1,21 @@
+import { ENDPOINTS } from "@/lib/api/endpoints"
+import privateClient from "@/lib/api/privateClient"
+
+export const supplyService = {
+  // READ
+  getAll: async () => {
+    const response = await privateClient.get(ENDPOINTS.SUPPLIES.GET_ALL)
+    return response.data
+  },
+
+  getById: async (id) => {
+    const response = await privateClient.get(ENDPOINTS.SUPPLIES.GET_BY_ID(id))
+    return response.data
+  },
+
+  // CREATE
+  create: async (data) => {
+    const response = await privateClient.post(ENDPOINTS.SUPPLIES.CREATE, data)
+    return response.data
+  },
+}
