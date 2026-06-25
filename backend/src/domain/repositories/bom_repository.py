4ae5156f63f075583fe763f1f0d7d2de8840437a
Chumbita <chumbitalexis@gmail.com>
2026-06-25 17:36:09 -0300

    refact: se redefinió la interfaz correpondiente a 'bom'.

--- backend/src/domain/repositories/bom_repository.py
+++ backend/src/domain/repositories/bom_repository.py
@@ -1,21 +1,15 @@
-from typing import Protocol, Optional, List
-from src.domain.entities.bom import Bom, BomItem
+# ══════════════════════════════════════════════════════════════════════════════
+# INTERFAZ DE BOM
+# ══════════════════════════════════════════════════════════════════════════════
+
+from typing import Protocol, Optional
+from src.domain.entities.bom import Bom
+
 
 class IBomRepository(Protocol):
-    async def create(self, bom: Bom) -> Bom:
-        ...
-    
-    async def update(self, bom: Bom) -> Bom:
-        ...
-    
-    async def delete(self, id: int) -> bool:
-        ...
-    
-    async def find_active_bom(self) -> List[Bom]:
-        ...
-    
-    async def find_all_bom(self) -> List[Bom]:
-        ...
-    
-    async def find_by_id(self, id: int) ->  Optional[Bom]:
+
+    async def get_by_id(self, bom_id: int) -> Optional[Bom]:
+        """
+        Obtiene un BOM por su ID.
+        """
         ...
