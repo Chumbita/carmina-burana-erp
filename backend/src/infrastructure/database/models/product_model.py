e744a7767cfa8ffca2261ce07d84b0bf6a098ffc
Carlos Olivera <carlitosolivera1510@gmail.com>
2026-07-02 00:44:23 -0300

    feat: Actualización del modelo Product.

--- backend/src/infrastructure/database/models/product_model.py
+++ backend/src/infrastructure/database/models/product_model.py
@@ -1,14 +1,14 @@
-from sqlalchemy import Column, String, Integer, Enum, Boolean, DateTime
-from sqlalchemy.sql import func
+from sqlalchemy import Column, BigInteger, SmallInteger, Numeric, Integer, TIMESTAMP, ForeignKey
 
 from src.infrastructure.database.base import Base
 
+
 class ProductModel(Base):
-    __tablename__ = "products"
-    
-    id = Column(Integer, primary_key=True)
-    name = Column(String(255), nullable=False)
-    product_type = Column(Enum("BASE_BEER", "PACKAGED", "KEG", name="products_types"), nullable=False)
-    unit = Column(String(10), nullable=False)
-    is_active = Column(Boolean, nullable=False, default=True)
-    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    __tablename__ = "product"
+
+    item_id      = Column(BigInteger,     ForeignKey("item.id"), primary_key=True, nullable=False)
+    product_type_id = Column(SmallInteger,   ForeignKey("product_type.id"), nullable=False)
+    net_content  = Column(Numeric(14, 4), nullable=False)
+    packaging_size = Column(Integer,      nullable=False)
+    created_at   = Column(TIMESTAMP,      nullable=False)
+    updated_at   = Column(TIMESTAMP,      nullable=True)
