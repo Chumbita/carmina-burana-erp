01e770660a93892f2157a88d588629efd975efc5
tomas14k <tomyamh123@gmail.com>
2026-07-02 19:01:48 -0300

    feat(presentation): agrega schemas, DI y endpoint POST /brands

--- backend/src/presentation/dependencies/use_cases/brand.py
+++ backend/src/presentation/dependencies/use_cases/brand.py
@@ -5,6 +5,7 @@
 from fastapi import Depends
 
 from src.application.use_cases.brand.get_all_brands_use_case import GetAllBrandsUseCase
+from src.application.use_cases.brand.create_brand import CreateBrandUseCase
 from src.presentation.dependencies.repositories import get_brand_respository
 from src.domain.repositories.brand_repository import IBrandRepository
 
@@ -13,3 +14,9 @@ def get_all_brands_use_case(
     repository: IBrandRepository = Depends(get_brand_respository)
 ) -> GetAllBrandsUseCase:
     return GetAllBrandsUseCase(repository)
+
+# ── CREATE BRAND ────────────────────────────────────────────────
+def get_create_brand_use_case(
+    repository: IBrandRepository = Depends(get_brand_respository)
+) -> CreateBrandUseCase:
+    return CreateBrandUseCase(repository)
