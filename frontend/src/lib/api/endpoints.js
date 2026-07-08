a07f4ec5916f04eff24c6ea5503edc9e760afcef
Carlos Olivera <carlitosolivera1510@gmail.com>
2026-07-08 19:27:36 -0300

    feat: Seagregó el endpoint de item para implementarlo en el frontend

--- frontend/src/lib/api/endpoints.js
+++ frontend/src/lib/api/endpoints.js
@@ -70,4 +70,8 @@ export const ENDPOINTS = {
   PRODUCTION_ORDERS: {
     CREATE: "/production-orders",
   },
-}
+  // Items
+  ITEMS: {
+    GET_MANUFACTURABLE: "/items/manufacturable-items",
+  },
+};
