3593351a5fdfdf62125528693086f8c748383e17
Carlos Olivera <carlitosolivera1510@gmail.com>
2026-07-09 21:23:22 -0300

    feat: Mostrar sufijos % y días en BeerTable

--- frontend/src/features/Inventario/beer/components/BeerTable.jsx
+++ frontend/src/features/Inventario/beer/components/BeerTable.jsx
@@ -1,16 +1,33 @@
 import { DataTable } from "@/components/shared/DataTable";
 
 export function BeerTable({ beer }) {
-  const tableHeaders = [
-    { header: "Nro", accessor: "id" },
-    { header: "Nombre", accessor: "name" },
-    { header: "Estilo", accessor: "style" },
-    { header: "ABV", accessor: "abv" },
-    { header: "IBU", accessor: "ibu" },
-    { header: "Fermentación", accessor: "fermentation_days" },
-    { header: "Acondicionamiento", accessor: "conditioning_days" },
-    { header: "Stock mínimo", accessor: "min_stock_level" },
-  ];
+const tableHeaders = [
+  { header: "Nro", accessor: "id" },
+  { header: "Nombre", accessor: "name" },
+  { header: "Estilo", accessor: "style" },
+  { 
+    header: "ABV", 
+    accessor: "abv", 
+    render: (value) => `${value}%` 
+  },
+  { 
+    header: "IBU", 
+    accessor: "ibu",
+    render: (value) => `${value}%`
+  },
+  { 
+    header: "Fermentación", 
+    accessor: "fermentation_days", 
+    render: (value) => `${value} días`
+  },
+  { 
+    header: "Acondicionamiento", 
+    accessor: "conditioning_days", 
+    render: (value) => `${value} días` 
+  },
+  { header: "Stock mínimo", accessor: "min_stock_level" },
+];
+
 
   return <DataTable columns={tableHeaders} data={beer} />;
 }
