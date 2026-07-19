8d921d3a5c57cc34c51738b42eae5c5715d18f4c
Carlos Olivera <carlitosolivera1510@gmail.com>
2026-07-19 11:55:11 -0300

    feat: Se agregaron al servicio de producción los endpoints para , release, start y complete.

--- frontend/src/features/production/cookings/services/productionService.js
+++ frontend/src/features/production/cookings/services/productionService.js
@@ -6,6 +6,26 @@ export const productionService = {
     const response = await privateClient.post(ENDPOINTS.PRODUCTION_ORDERS.CREATE, data)
     return response.data
   },
+
+  getIncomplete: async () => {
+    const response = await privateClient.get(ENDPOINTS.PRODUCTION_ORDERS.GET_INCOMPLETE)
+    return response.data 
+  },
+
+  release: async (orderId) => {
+    const response = await privateClient.post(ENDPOINTS.PRODUCTION_ORDERS.RELEASE(orderId))
+    return response.data
+  },
+
+  start: async (orderId) => {
+    const response = await privateClient.post(ENDPOINTS.PRODUCTION_ORDERS.START(orderId))
+    return response.data
+  },
+
+  complete: async (orderId, data) => {
+    const response = await privateClient.post(ENDPOINTS.PRODUCTION_ORDERS.COMPLETE(orderId), data)
+    return response.data
+  }
 }
 
 export default productionService
